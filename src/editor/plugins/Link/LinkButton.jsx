292d3419d5ddb420afcdfb908726c28cb8b09c37
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-05-13 10:21:36 +0300

    Load buttons from settings

--- src/editor/plugins/Link/LinkButton.jsx
+++ src/editor/plugins/Link/LinkButton.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSlate } from 'slate-react';
 import Button from './../../components/Button';
 import { isLinkActive, insertLink } from './utils';
+import linkSVG from '@plone/volto/icons/link.svg';
+import unlinkSVG from '@plone/volto/icons/unlink.svg';
 
 const LinkButton = () => {
   const editor = useSlate();
@@ -14,9 +16,8 @@ const LinkButton = () => {
         if (!url) return;
         insertLink(editor, url);
       }}
-    >
-      Link
-    </Button>
+      icon={linkSVG}
+    ></Button>
   );
 };
 export default LinkButton;

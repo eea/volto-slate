a35b61d1abaccc2c9f50278300b37e5c54e05489
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-05-21 16:02:46 +0300

    Move Link element to render module

---
+++ src/editor/plugins/Link/render.jsx
@@ -0,0 +1,9 @@
+export const LinkElement = ({ attributes, children, element }) => {
+  // TODO: use the element.data to decide how to compose the link
+  const { title } = element?.data || {};
+  return (
+    <a {...attributes} href={element.url} title={title}>
+      {children}
+    </a>
+  );
+};
--- src/editor/plugins/Markdown/index.js
+++ src/editor/plugins/Markdown/index.js
@@ -3,6 +3,7 @@ import { withShortcuts } from './decorators';
 export default function install(config) {
   const slate = config.settings.slate || {};
   config.settings.slate = slate;
+
   slate.decorators = [...(slate.decorators || []), withShortcuts];
 
   return config;

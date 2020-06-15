458cc54ab82e78535daed825ff0def732c6ab53c
Silviu Bogan <silviubogan@gmail.com>
2020-06-15 14:50:59 +0300

    Custom event for selecting in Slate from Cypress test

--- src/editor/components/SlateEditor.jsx
+++ src/editor/components/SlateEditor.jsx
@@ -110,6 +110,41 @@ const SlateEditor = ({
     },
   ];
 
+  // Source: https://stackoverflow.com/a/53623568/258462
+  const onTestSelectWord = (val) => {
+    let slateEditor =
+      val.detail.parentElement.parentElement.parentElement.parentElement;
+
+    // Events are special, can't use spread or Object.keys
+    let selectEvent = {};
+    for (let key in val) {
+      if (key === 'currentTarget') {
+        selectEvent['currentTarget'] = slateEditor;
+      } else if (key === 'type') {
+        selectEvent['type'] = 'select';
+      } else {
+        selectEvent[key] = val[key];
+      }
+    }
+
+    // Make selection
+    let selection = window.getSelection();
+    let range = document.createRange();
+    range.selectNodeContents(val.detail);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    // Slate monitors DOM selection changes automatically
+  };
+
+  React.useEffect(() => {
+    document.addEventListener('Test_SelectWord', onTestSelectWord);
+
+    return () => {
+      document.removeEventListener('Test_SelectWord', onTestSelectWord);
+    };
+  });
+
   return (
     <div
       data-slate-value={window?.Cypress ? JSON.stringify(value, null, 2) : null}

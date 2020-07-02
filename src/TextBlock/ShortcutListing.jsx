e78caa99e8f2da037020548b24c8d136a1462107
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-07-02 07:08:45 +0300

    Improve styling of shortcuts block; restructured elements definitions

--- src/TextBlock/ShortcutListing.jsx
+++ src/TextBlock/ShortcutListing.jsx
@@ -5,15 +5,20 @@ import React from 'react';
 const ShortcutListing = (props) => {
   const hotkeys = settings?.slate?.hotkeys;
   return (
-    <Segment>
-      <h3>Text editor shortcuts</h3>
-      <List>
-        {hotkeys &&
-          Object.entries(hotkeys).map(([shortcut, format]) => (
-            <List.Item key={shortcut}>{`${shortcut}: ${format}`}</List.Item>
-          ))}
-      </List>
-    </Segment>
+    <Segment.Group raised className="form">
+      <header className="header pulled">
+        <h2>Text editor shortcuts</h2>
+      </header>
+
+      <Segment secondary attached>
+        <List>
+          {hotkeys &&
+            Object.entries(hotkeys).map(([shortcut, format]) => (
+              <List.Item key={shortcut}>{`${shortcut}: ${format}`}</List.Item>
+            ))}
+        </List>
+      </Segment>
+    </Segment.Group>
   );
 };
 
--- src/editor/render.jsx
+++ src/editor/render.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Node } from 'slate';
 import { settings } from '~/config';
 
+// TODO: read, see if relevant
+// https://reactjs.org/docs/higher-order-components.html#dont-use-hocs-inside-the-render-method
 export const Element = (props) => {
   const { element } = props; // attributes, children,
   const { elementRenderers } = settings.slate;
-  const AddonEl = elementRenderers[element.type] || elementRenderers['default'];
+  const El = elementRenderers[element.type] || elementRenderers['default'];
 
-  return <AddonEl {...props} />;
+  return <El {...props} />;
 };
 
 export const Leaf = ({ attributes, leaf, children }) => {
@@ -23,30 +25,3 @@ export const Leaf = ({ attributes, leaf, children }) => {
 export const plaintext_serialize = (nodes) => {
   return nodes.map((n) => Node.string(n)).join('\n');
 };
-
-// import { useSelected, useFocused } from 'slate-react';
-// const selected = useSelected();
-// const focused = useFocused();
-// case 'image':
-//   const type = attributes['data-slate-type'];
-//   delete attributes['data-slate-type'];
-
-//   return (
-//     // <div {...attributes}>
-//     <div {...attributes} contentEditable={false}>
-//       <img
-//         data-slate-type={type}
-//         src={element.url}
-//         alt=""
-//         style={{
-//           display: 'block',
-//           maxWidth: '100%',
-//           maxHeight: '20em',
-//           padding: '10px 0',
-//           boxShadow: selected && focused ? '0 0 0 3px #B4D5FF' : 'none',
-//         }}
-//       />
-//     </div>
-//     // {/* {children} */}
-//     // </div>
-//   );

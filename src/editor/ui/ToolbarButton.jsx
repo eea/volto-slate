560c946ba7b08f450c64ca4fcb973a0e6f585bff
Silviu Bogan <silviubogan@gmail.com>
2020-07-03 12:59:17 +0300

    Improved toolbar styling to be like in draft blocks

--- src/editor/ui/ToolbarButton.jsx
+++ src/editor/ui/ToolbarButton.jsx
@@ -6,19 +6,21 @@ import { Button } from 'semantic-ui-react';
 const ToolbarButton = React.forwardRef(
   ({ className, active, reversed, icon, style, ...props }, ref) => {
     return (
-      <Button
-        as="a"
-        {...props}
-        ref={ref}
-        style={style}
-        className={cx(className)}
-        active={active}
-        inverted={reversed}
-        compact
-        toggle
-        size="tiny"
-        icon={icon && <Icon name={icon} size="24px" />}
-      ></Button>
+      <div className="button-wrapper">
+        <Button
+          as="a"
+          {...props}
+          ref={ref}
+          style={style}
+          className={cx(className)}
+          active={active}
+          inverted={reversed}
+          compact
+          toggle
+          size="tiny"
+          icon={icon && <Icon name={icon} size="24px" />}
+        ></Button>
+      </div>
     );
   },
 );

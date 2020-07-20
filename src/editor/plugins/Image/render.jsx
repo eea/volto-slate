75dbaa3f3317047b958c08693d50b3fb586e1f53
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-07-21 00:05:23 +0300

    Working image plugin

--- src/editor/plugins/Image/render.jsx
+++ src/editor/plugins/Image/render.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useSelected, useFocused } from 'slate-react';
 
-export const ImageElement = ({ attributes, children, element }) => {
-  console.log('image el', attributes, children, element);
-  return <img {...attributes} src={element.src} alt="" />;
+export const ImageElement = (props) => {
+  const { attributes, children, element } = props;
+  const selected = useSelected();
+  const focused = useFocused();
+
+  const style = {
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '20em',
+    boxShadow: selected && focused ? '0 0 0 2px blue' : 'none',
+  };
+
+  return (
+    <div {...attributes}>
+      {children}
+      <img alt="" src={element.src} style={style} />
+    </div>
+  );
 };

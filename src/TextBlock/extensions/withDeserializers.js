112bc80e90066144bc2672b803b529aa57b893cf
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-07-27 19:12:07 +0300

    WIP on paste handling

--- src/TextBlock/extensions/withDeserializers.js
+++ src/TextBlock/extensions/withDeserializers.js
@@ -1,3 +1,5 @@
+import { blockTagDeserializer } from 'volto-slate/editor/deserialize';
+
 // import { settings } from '~/config';
 
 // export const deserializeListTag = (tagname) => (editor, el) => {
@@ -13,6 +15,7 @@
 export const withDeserializers = (editor) => {
   editor.htmlTagsToSlate = {
     ...editor.htmlTagsToSlate,
+    H1: blockTagDeserializer('h2'),
     // UL: deserializeListTag('ul'),
     // OL: deserializeListTag('ol'),
     // LI: deserializeListItemTag,
--- src/editor/extensions/insertData.js
+++ src/editor/extensions/insertData.js
@@ -19,28 +19,33 @@ export const insertData = (editor) => {
       const fragment = deserialize(editor, parsed.body);
       console.log('parsed', parsed, fragment);
 
-      // Delete the empty placeholder paragraph, if we can
-      const isEmpty = !Editor.string(editor, []);
-      const isTextFragment =
-        Array.isArray(fragment) &&
-        fragment.length === 1 &&
-        (Editor.isInline(fragment[0]) || Text.isText(fragment[0]));
-      if (isEmpty && !isTextFragment) {
-        console.log(
-          'deletenodes',
-          isTextFragment,
-          fragment,
-          Editor.isInline(fragment[0]),
-        );
+      if (!Editor.string(editor, [])) {
+        // Delete the empty placeholder paragraph, if we can
+        Transforms.deselect(editor);
         Transforms.removeNodes(editor);
       }
+
+      // const isTextFragment =
+      //   Array.isArray(fragment) &&
+      //   fragment.length === 1 &&
+      //   (Editor.isInline(fragment[0]) || Text.isText(fragment[0]));
 
       // TODO: insertNodes works a lot better then insertFragment (needs less cleanup)
       // but insertFragment is more reliable to get content inserted
       // We can't afford to insert a fragment, we want Slate to clean up
       // Editor.insertFragment(editor, fragment);
       // Transforms.insertFragment(editor, fragment);
-      Transforms.deselect(editor);
+
+      // if (isEmpty && !isTextFragment) {
+      //   console.log(
+      //     'deletenodes',
+      //     isTextFragment,
+      //     fragment,
+      //     Editor.isInline(fragment[0]),
+      //   );
+      //   // Transforms.removeNodes(editor);
+      // }
+
       Transforms.insertNodes(editor, fragment);
       Transforms.deselect(editor);
 

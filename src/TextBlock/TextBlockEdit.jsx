07bdb7034b09a6ec4187df813e87170926a00739
Alin Voinea <contact@avoinea.com>
2020-07-27 10:53:19 +0300

    Try to get placeholder from data

--- src/TextBlock/TextBlockEdit.jsx
+++ src/TextBlock/TextBlockEdit.jsx
@@ -189,7 +189,7 @@ const TextBlockEdit = (props) => {
             }}
             onKeyDown={handleKey}
             selected={selected}
-            placeholder="Enter some rich text…"
+            placeholder={data.placeholder || "Enter some rich text…"}
           />
         )}
       </Dropzone>

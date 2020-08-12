c257200817caccdd455c0f1be75b54e49755eb29
Silviu Bogan <silviubogan@gmail.com>
2020-08-12 17:02:31 +0300

    The Slate-based Title component is working

--- src/blocks/Title/TitleBlockEdit.jsx
+++ src/blocks/Title/TitleBlockEdit.jsx
@@ -3,25 +3,10 @@
  * @module volto-slate/blocks/Title/TitleBlockEdit
  */
 
-import React, {
-  useEffect,
-  useContext,
-  useCallback,
-  useRef,
-  useMemo,
-  useState,
-} from 'react';
+import React, { useContext, useCallback, useMemo } from 'react';
 import { Editor, createEditor } from 'slate';
-import {
-  ReactEditor,
-  Editable,
-  Slate,
-  withReact,
-  useEditor,
-  useFocused,
-} from 'slate-react';
-import cx from 'classnames';
-
+import { ReactEditor, Editable, Slate, withReact } from 'slate-react';
+import { fixSelection } from 'volto-slate/utils';
 import PropTypes from 'prop-types';
 import { defineMessages, useIntl } from 'react-intl';
 import { settings } from '~/config';
@@ -44,7 +29,6 @@ export const TitleBlockEdit = ({
   index,
   onChangeField,
   onSelectBlock,
-  // onDeleteBlock,
   onAddBlock,
   onFocusPreviousBlock,
   onFocusNextBlock,
@@ -52,28 +36,43 @@ export const TitleBlockEdit = ({
   blockNode,
 }) => {
   const editor = useMemo(() => withReact(createEditor()), []);
-  // const focused = useFocused();
   const intl = useIntl();
   const formContext = useContext(FormStateContext);
-  useEffect(() => {
-    if (selected) {
-      ReactEditor.focus(editor);
-    }
-  });
-  useEffect(() => {
-    if (selected) {
-      ReactEditor.focus(editor);
-    }
-  }, [selected]);
   const handleChange = useCallback(() => {
     onChangeField('title', Editor.string(editor, []));
   }, [editor, onChangeField]);
 
+  // TODO: move the code below, copied from SlateEditor component, into a custom hook that is called from both places
+  /*
+   * We 'restore' the selection because we manipulate it in several cases:
+   * - when blocks are artificially joined, we set the selection at junction
+   * - when moving up, we set it at end of previous blok
+   * - when moving down, we set it at beginning of next block
+   */
+  React.useLayoutEffect(() => {
+    if (selected) {
+      ReactEditor.focus(editor);
+
+      // This makes the Backspace key work properly in block.
+      // Don't remove it, unless this test passes:
+      // - with the Slate block unselected, click in the block.
+      // - Hit backspace. If it deletes, then the test passes
+      fixSelection(editor);
+
+      // if (defaultSelection) {
+      //   if (initial_selection.current !== defaultSelection) {
+      //     initial_selection.current = defaultSelection;
+      //     setTimeout(() => Transforms.select(editor, defaultSelection), 0);
+      //   }
+      //   return () => ReactEditor.blur(editor);
+      // }
+    }
+    return () => ReactEditor.blur(editor);
+  }, [editor, selected]);
+
   if (__SERVER__) {
     return <div />;
   }
-
-  console.log('SELECTED ?', selected);
 
   return (
     <Slate
@@ -87,7 +86,6 @@ export const TitleBlockEdit = ({
       ]}
     >
       <Editable
-        // className={cx({ selected: true })}
         onKeyDown={(ev) => {
           if (ev.key === 'Return' || ev.key === 'Enter') {
             ev.preventDefault();
@@ -109,6 +107,9 @@ export const TitleBlockEdit = ({
               {children}
             </h1>
           );
+        }}
+        onFocus={() => {
+          onSelectBlock(block);
         }}
       ></Editable>
     </Slate>

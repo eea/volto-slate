c56ac30881547562ff275b4ac4dd6a51b14c79b8
Silviu Bogan <silviubogan@gmail.com>
2020-08-17 14:47:34 +0300

    Added a doc comment

--- src/blocks/Footnote/FootnotesBlockEdit.jsx
+++ src/blocks/Footnote/FootnotesBlockEdit.jsx
@@ -5,6 +5,10 @@ import { FootnoteBlockSchema as schema } from './schema';
 import { SidebarPortal } from '@plone/volto/components';
 import { useFormStateContext } from '@plone/volto/components/manage/Form/FormContext';
 
+/**
+ * @summary The React component that allows the footnotes block view to be edited using a form in a sidebar portal.
+ * @param {object} props Contains the props received by any Edit component of a registered Volto block: `selected`, `block`, `data`, `onChangeBlock` etc.
+ */
 const FootnotesBlockEdit = (props) => {
   const { selected, block, data, onChangeBlock } = props;
   const { contextData } = useFormStateContext();

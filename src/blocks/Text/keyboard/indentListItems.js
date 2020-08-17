3d7a50768c852e5664d74d51952781a26be691b9
Silviu Bogan <silviubogan@gmail.com>
2020-08-17 19:58:27 +0300

    Improved comments

--- src/blocks/Text/keyboard/indentListItems.js
+++ src/blocks/Text/keyboard/indentListItems.js
@@ -10,8 +10,8 @@ import { settings } from '~/config';
 
 /**
  * @function indentListItems
- * @param editor {Object}
- * @param event {Object}
+ * @param {Editor} editor
+ * @param {Event} event
  *
  * This bit is quite involved. You need a good understanding of Slate API and
  * Slate's DOM-like behaviour.
@@ -38,25 +38,36 @@ import { settings } from '~/config';
  */
 export function indentListItems({ editor, event }) {
   // TODO: test if the cursor is at the beginning of the list item
-  if (!isCursorInList(editor)) return;
 
-  event.preventDefault();
-  event.stopPropagation();
+  // If the text cursor is not in a list, then do nothing.
+  if (!isCursorInList(editor)) {
+    return;
+  } else {
+    // Else prevent the default event handling and bubbling.
+    event.preventDefault();
+    event.stopPropagation();
 
-  return event.shiftKey
-    ? event.ctrlKey
-      ? decreaseMultipleItemsDepth(editor, event)
-      : decreaseItemDepth(editor, event)
-    : event.ctrlKey
-    ? increaseMultipleItemDepth(editor, event)
-    : increaseItemDepth(editor, event);
+    // If Shift & Ctrl, decrease multiple items depth.
+    // If Shift & !Ctrl, decrease item depth.
+    // If !Shift & Ctrl, increase multiple item depth.
+    // If !Shift & !Ctrl, increase item depth.
+    return event.shiftKey
+      ? event.ctrlKey
+        ? decreaseMultipleItemsDepth(editor, event)
+        : decreaseItemDepth(editor, event)
+      : event.ctrlKey
+      ? increaseMultipleItemDepth(editor, event)
+      : increaseItemDepth(editor, event);
+  }
 }
 
 /**
  * @function decreaseItemDepth
  *
- * @param {} editor
- * @param {} event
+ * Decreases indent level of a single list item.
+ *
+ * @param {Editor} editor
+ * @param {Event} event
  */
 export function decreaseItemDepth(editor, event) {
   const { slate } = settings;
@@ -71,6 +82,7 @@ export function decreaseItemDepth(editor, event) {
   // with it as a sublist
   const listItemRef = Editor.pathRef(editor, listItemPath);
 
+  // TODO: please clarify, we unwrap at list item path but we only unwrap nodes that match list types, but list item type is different from all list types:
   Transforms.unwrapNodes(editor, {
     at: listItemPath,
     split: true,
@@ -78,13 +90,25 @@ export function decreaseItemDepth(editor, event) {
     match: (node) => slate.listTypes.includes(node.type),
   });
 
+  /**
+   * This condition is the same as "listItemRef.current is not the root editor node or one if its children".
+   */
+  function getCondition1() {
+    return listItemRef.current.length > 1;
+  }
+
+  /**
+   * @returns The current parent Node of the PathRef linked to the initial list item that we want deindented.
+   */
+  function getParent() {
+    return Path.parent(listItemRef.current);
+  }
+
   // Merge with any previous <ul/ol> list
-  if (listItemRef.current.length > 1)
-    mergeWithPreviousList(editor, Path.parent(listItemRef.current));
+  if (getCondition1()) mergeWithPreviousList(editor, getParent());
 
   // Merge with any next <ul/ol> list
-  if (listItemRef.current.length > 1)
-    mergeWithNextList(editor, Path.parent(listItemRef.current));
+  if (getCondition1()) mergeWithNextList(editor, getParent());
 
   if (parentListPath.length === 1) {
     // Our parent is at root, unwrapping list item, user wants to break out

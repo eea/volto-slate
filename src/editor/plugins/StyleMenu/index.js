3d21fd13420b677b8492c11e491a39b9d22bcfea
Silviu Bogan <silviubogan@gmail.com>
2020-09-01 16:17:24 +0300

    Clarified example styles

--- src/editor/plugins/StyleMenu/index.js
+++ src/editor/plugins/StyleMenu/index.js
@@ -13,8 +13,8 @@ export default function install(config) {
   ];
 
   slate.styleMenuDefinitions = [
-    { cssClass: 'green-text', isBlock: true, label: 'Green Text' },
-    { cssClass: 'blue-text', isBlock: false, label: 'Blue Text' },
+    { cssClass: 'green-block-text', isBlock: true, label: 'Green Text' },
+    { cssClass: 'cool-inline-text', isBlock: false, label: 'Cool Inline Text' },
     { cssClass: 'red-inline-text', isBlock: false, label: 'Red Inline Text' },
   ];
 

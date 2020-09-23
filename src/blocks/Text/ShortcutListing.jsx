897c455b6afdf58c187a26cbd8c966fb2d37b98a
Silviu Bogan <silviubogan@gmail.com>
2020-09-23 19:35:52 +0300

    Add a doc comment & a TODO comment

--- src/blocks/Text/ShortcutListing.jsx
+++ src/blocks/Text/ShortcutListing.jsx
@@ -2,6 +2,11 @@ import { Segment, List } from 'semantic-ui-react';
 import { settings } from '~/config';
 import React from 'react';
 
+/**
+ * Displays the registered hotkeys for keyboard as they are in the settings.
+ *
+ * @param {object} props Unused.
+ */
 const ShortcutListing = (props) => {
   const hotkeys = settings?.slate?.hotkeys;
   return (
@@ -16,6 +21,9 @@ const ShortcutListing = (props) => {
             <List.Item key={shortcut}>{`${shortcut}: ${format}`}</List.Item>
           ))}
         </List>
+        {/* TODO: Eventually detect the OS automatically from the user agent
+            and display just the relevant information in the following
+            element: */}
         <div>On Windows, the MOD key is Ctrl, on MacOSX it's Cmd.</div>
       </Segment>
     </div>

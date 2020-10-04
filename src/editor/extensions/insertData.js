c2bd4fd0b32501c4117f777bed5dd41860d6e7f7
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-10-04 11:59:04 +0300

    Better handling of paste data coming from slate

--- src/editor/extensions/insertData.js
+++ src/editor/extensions/insertData.js
@@ -1,49 +1,37 @@
-import { Editor, Text, Transforms, Block } from 'slate';
+import { Editor, Text, Transforms } from 'slate';
 import { deserialize } from 'volto-slate/editor/deserialize';
-import { deconstructToVoltoBlocks } from 'volto-slate/utils';
-import { settings } from '~/config';
 
-/**
- * @param {Text} textNode The (leaf) Text node to wrap.
- *
- * @returns {Block} A Slate block node, of the default block type configured in the Slate settings, containing the given Text node.
- */
-function createBlock(textNode) {
-  return {
-    type: settings.slate.defaultBlockType,
-    children: [textNode],
-  };
-}
-
-/**
- * @summary Inserts in the given editor the feature of being able to paste HTML content in it.
- *
- * @param {Editor} editor A Slate editor object.
- */
 export const insertData = (editor) => {
   const { insertData } = editor;
 
-  // console.log('insertData extension here ');
   editor.insertData = (data) => {
     console.log('data in custom editor.insertData', data);
     // const text = data.getData('text/rtf');
-    // console.log('text', text);
+
+    let fragment;
+
     const html = data.getData('text/html');
+    fragment = data.getData('application/x-slate-fragment');
+
+    if (fragment) {
+      const decoded = decodeURIComponent(window.atob(fragment));
+      const parsed = JSON.parse(decoded);
+      editor.insertFragment(parsed);
+      return;
+    }
 
     if (html) {
       const parsed = new DOMParser().parseFromString(html, 'text/html');
 
-      let body;
-      if (parsed.getElementsByTagName('google-sheets-html-origin').length > 0) {
-        body = parsed.querySelector('google-sheets-html-origin > table');
-      } else {
-        body = parsed.body;
-      }
+      const body =
+        parsed.getElementsByTagName('google-sheets-html-origin').length > 0
+          ? parsed.querySelector('google-sheets-html-origin > table')
+          : parsed.body;
 
+      fragment = deserialize(editor, body);
       console.log('deserialize body', body);
-      let fragment = deserialize(editor, body);
       console.log('parsed body', parsed);
-      console.log('parse fragment', fragment);
+      console.log('parsed fragment', fragment);
 
       // If there is no text in the editor
       if (!Editor.string(editor, [])) {

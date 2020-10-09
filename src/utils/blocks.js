20bb171e62492e9071d9e98f2317bcb0e728d306
Silviu Bogan <silviubogan@gmail.com>
2020-10-09 12:33:16 +0300

    Block style buttons work again

--- src/utils/blocks.js
+++ src/utils/blocks.js
@@ -1,5 +1,6 @@
 import { Editor, Transforms, Text } from 'slate'; // Range, RangeRef
 import { settings } from '~/config';
+import { deconstructToVoltoBlocks } from 'volto-slate/utils';
 
 /**
  * Is it text? Is it whitespace (space, newlines, tabs) ?
@@ -78,4 +79,89 @@ export const toggleInlineFormat = (editor, format) => {
   }
   const block = { type: format, children: [] };
   Transforms.wrapNodes(editor, block, { split: true });
+};
+
+export const toggleBlock = (editor, format) => {
+  // We have 6 boolean variables which need to be accounted for.
+  // See https://docs.google.com/spreadsheets/d/1mVeMuqSTMABV2BhoHPrPAFjn7zUksbNgZ9AQK_dcd3U/edit?usp=sharing
+  const { slate } = settings;
+  const { listTypes } = slate;
+
+  const isListItem = isBlockActive(editor, slate.listItemType);
+  const isActive = isBlockActive(editor, format);
+  const wantsList = listTypes.includes(format);
+
+  if (isListItem && !wantsList) {
+    toggleFormatAsListItem(editor, format);
+  } else if (isListItem && wantsList && !isActive) {
+    switchListType(editor, format); // this will deconstruct to Volto blocks
+  } else if (!isListItem && wantsList) {
+    changeBlockToList(editor, format);
+  } else if (!isListItem && !wantsList) {
+    toggleFormat(editor, format);
+  } else {
+    console.warn('toggleBlock case not covered, please examine:', {
+      wantsList,
+      isActive,
+      isListItem,
+    });
+  }
+};
+
+/*
+ * Applies a block format unto a list item. Will split the list and deconstruct the
+ * block
+ */
+export const toggleFormatAsListItem = (editor, format) => {
+  const { slate } = settings;
+  Transforms.unwrapNodes(editor, {
+    match: (n) => slate.listTypes.includes(n.type),
+    split: true,
+  });
+
+  Transforms.setNodes(editor, {
+    type: format,
+  });
+
+  deconstructToVoltoBlocks(editor);
+};
+
+/*
+ * Toggles between list types by exploding the block
+ */
+export const switchListType = (editor, format) => {
+  const { slate } = settings;
+  Transforms.unwrapNodes(editor, {
+    match: (n) => slate.listTypes.includes(n.type),
+    split: true,
+  });
+  const block = { type: format, children: [] };
+  Transforms.wrapNodes(editor, block);
+
+  deconstructToVoltoBlocks(editor);
+};
+
+export const changeBlockToList = (editor, format) => {
+  const { slate } = settings;
+  const [match] = Editor.nodes(editor, {
+    match: (n) => n.type === slate.listItemType,
+  });
+
+  if (!match) {
+    Transforms.setNodes(editor, {
+      type: slate.listItemType,
+      // id: nanoid(8),
+    });
+  }
+  const block = { type: format, children: [] };
+  Transforms.wrapNodes(editor, block);
+};
+
+export const toggleFormat = (editor, format) => {
+  const { slate } = settings;
+  const isActive = isBlockActive(editor, format);
+  const type = isActive ? slate.defaultBlockType : format;
+  Transforms.setNodes(editor, {
+    type,
+  });
 };

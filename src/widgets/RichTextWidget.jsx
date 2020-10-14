ed7b35bc3e1a91b4c88dc498a560313048fb11ca
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-10-14 21:25:19 +0300

    Add style to the richtext widget

--- src/widgets/RichTextWidget.jsx
+++ src/widgets/RichTextWidget.jsx
@@ -7,12 +7,15 @@ import React from 'react';
 import { FormFieldWrapper } from '@plone/volto/components';
 import SlateEditor from 'volto-slate/editor/SlateEditor';
 
+import './style.css';
+
 const SlateRichTextWidget = (props) => {
   const { id, onChange, value, focus } = props;
   const [selected, setSelected] = React.useState(focus);
   return (
     <FormFieldWrapper {...props} draggable={false} className="slate_wysiwyg">
       <div
+        className="slate_wysiwyg_box"
         role="textbox"
         tabIndex="-1"
         style={{ boxSizing: 'initial' }}

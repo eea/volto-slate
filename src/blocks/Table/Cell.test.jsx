4c9a428bbfb1129c94b9a21b83b04c111e5813b5
Alin Voinea <contact@avoinea.com>
2020-11-12 14:38:50 +0200

    [JENKINS] - Update snapshot tests

--- src/blocks/Table/Cell.test.jsx
+++ src/blocks/Table/Cell.test.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Cell from './Cell';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-intl-redux';
+
+const mockStore = configureStore();
 
 global.__SERVER__ = true; // eslint-disable-line no-underscore-dangle
 
 test('renders a cell component', () => {
+  const store = mockStore({
+    intl: {
+      locale: 'en',
+      messages: {},
+    },
+  });
   const component = renderer.create(
-    <Cell onChange={() => {}} onSelectCell={() => {}} />,
+    <Provider store={store}>
+      <Cell onChange={() => {}} onSelectCell={() => {}} />,
+    </Provider>,
   );
   const json = component.toJSON();
   expect(json).toMatchSnapshot();

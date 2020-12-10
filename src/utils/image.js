ce4c50edb6c10605c4731ab06710deedc4cbba0b
Silviu Bogan <silviubogan@gmail.com>
2020-12-10 19:09:21 +0200

    Promisify srcToDataUri and correct a typo

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,25 +1,27 @@
-export function srcToDataUri(url, callback) {
+export function srcToDataUri(url) {
   // Based on https://github.com/ianstormtaylor/slate-plugins
   const canvas = window.document.createElement('canvas');
   const img = window.document.createElement('img');
 
-  if (!canvas.getContext) {
-    return setTimeout(callback, 0, new Error('Canvas is not supported.'));
-  }
+  return new Promise((resolve, reject) => {
+    if (!canvas.getContext) {
+      return reject(new Error('Canvas is not supported.'));
+    }
 
-  img.onload = () => {
-    const ctx = canvas.getContext('2d');
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
-    const dataUri = canvas.toDataURL('image/png');
-    callback(null, dataUri);
-  };
+    img.onload = () => {
+      const ctx = canvas.getContext('2d');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      const dataUri = canvas.toDataURL('image/png');
+      resolve(dataUri);
+    };
 
-  img.ononerror = () => {
-    callback(new Error('Failed to load image.'));
-  };
+    img.onerror = () => {
+      reject(new Error('Failed to load image.'));
+    };
 
-  img.setAttribute('crossOrigin', 'anonymous');
-  img.src = url;
+    img.setAttribute('crossorigin', 'anonymous');
+    img.src = url;
+  });
 }

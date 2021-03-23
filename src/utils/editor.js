aa8cd7eb8c77c32e9cd2fc555fba6c2372780d65
razvanMiu <miu.razvan28@gmail.com>
2021-03-23 14:23:58 +0200

    Export Editor

--- src/utils/editor.js
+++ src/utils/editor.js
@@ -1,6 +1,8 @@
-import { Transforms } from 'slate';
+import { Editor, Transforms } from 'slate';
 
 export function setEditorContent(editor, block) {
   Transforms.removeNodes(editor, { at: [0] }); // TODO: at: [] needs rethinking
   Transforms.insertNodes(editor, block);
 }
+
+export { Editor };

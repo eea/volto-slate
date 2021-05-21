32f805042016ed0473945084251bd41c02ed6ca2
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2021-05-21 19:53:54 +0300

    add '@babel/plugin-transform-modules-commonjs'

--- .coverage.babel.config.js
+++ .coverage.babel.config.js
@@ -2,7 +2,7 @@ const defaultBabel = require('@plone/volto/babel');
 
 function applyDefault(api) {
   const voltoBabel = defaultBabel(api);
-  voltoBabel.plugins.push('transform-class-properties', 'istanbul');
+  voltoBabel.plugins.push('@babel/plugin-transform-modules-commonjs', 'transform-class-properties', 'istanbul');
   return voltoBabel;
 }
 

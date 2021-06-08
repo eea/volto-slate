6b1300b88fa7bc15bc047c57e65d9df5bbe5144b
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-06-08 06:50:37 +0300

    Remove deprecated InlineForm

--- src/futurevolto/InlineForm.jsx
+++
@@ -1,6 +0,0 @@
-import { InlineForm } from '@plone/volto/components';
-// eslint-disable-next-line no-console
-console.warn(
-  'DEPRECATED: volto-slate - InlineForm. Moved to `@plone/volto/components`',
-);
-export default InlineForm;

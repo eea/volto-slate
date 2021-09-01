e4c7f9ddc6e67b18baaff0ea1379379055a32168
Alin Voinea <contact@avoinea.com>
2021-09-01 22:58:22 +0300

    Support new metadata fields on old content

--- src/widgets/RichTextWidget.jsx
+++ src/widgets/RichTextWidget.jsx
@@ -48,7 +48,7 @@ const SlateRichTextWidget = (props) => {
           name={id}
           value={
             typeof value === 'undefined' ||
-            typeof value.data !==
+            typeof value?.data !==
               'undefined' /* previously this was a Draft block */
               ? [createEmptyParagraph()]
               : value

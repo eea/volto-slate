3edf6a88604080091c70b560e03116b684945ad5
Alin Voinea <contact@avoinea.com>
2021-09-03 12:08:14 +0300

    Remove DEPRECATED Blocks helpers and SidebarPopup

--- src/futurevolto/SidebarPopup.jsx
+++
@@ -1,6 +0,0 @@
-import { SidebarPopup } from '@plone/volto/components';
-// eslint-disable-next-line no-console
-console.warn(
-  'DEPRECATED: volto-slate - SidebarPopup. Moved to `@plone/volto/components`',
-);
-export default SidebarPopup;

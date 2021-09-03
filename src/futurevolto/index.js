bc38505c656f689b4ca70eebd4fd29ad0cbfd491
Alin Voinea <contact@avoinea.com>
2021-09-03 12:19:29 +0300

    Remove DEPRECATED ObjectWidget from futurevolto. Moved to Volto core

--- src/futurevolto/index.js
+++ src/futurevolto/index.js
@@ -1,8 +1,6 @@
-// import ObjectWidget from './ObjectWidget';
 import { ObjectBrowserWidgetMode } from '@plone/volto/components/manage/Widgets/ObjectBrowserWidget';
 
 export default (config) => {
-  // config.widgets.widget.object = ObjectWidget;
   config.widgets.widget.object_browser = ObjectBrowserWidgetMode('link');
   return config;
 };

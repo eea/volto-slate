9ff66be6e053b244790d2b22510337931a8f4719
Silviu Bogan <silviubogan@gmail.com>
2021-11-02 10:03:06 +0200

    Revert "Upgrade Slate packages, WIP on Cypress paste test"
    This reverts commit 3d56e6970fc8cfced3ff285ace4050fb891b212b.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,44 +12,12 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-// TODO: make this plugin work:
-// https://stackoverflow.com/questions/69798980/how-to-use-the-blob-constructor-inside-a-cypress-plugin
-
-// NOTE: cannot upgrade to ESM-only clipboardy 3 since Cypress plugins must
-// still use require() to import packages.
-const clipboard = require('clipboardy');
-const clipboardPolyfill = require('clipboard-polyfill');
-// const Blob = require('blob-polyfill').Blob;
-
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-
-  on('task', {
-    // Clipboard test plugin
-    getClipboard: () => {
-      const cb = clipboard.readSync();
-      return cb;
-    },
-    setClipboardText: (s) => {
-      clipboard.writeSync(s);
-      return null;
-    },
-    setClipboardHtml: (s) => {
-      // cy.window((win) => {
-      console.log(global.Blob);
-      // });
-      return clipboardPolyfill.write([
-        new clipboardPolyfill.ClipboardItem({
-          'text/html': new Blob([s], { type: 'text/html' }),
-        }),
-      ]);
-    },
-  });
-
   /* coverage-start
   require('@cypress/code-coverage/task')(on, config)
   on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))

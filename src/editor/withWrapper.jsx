7982e68f7aa2c1906e4a271540eeaed5288be301
Silviu Bogan <silviubogan@gmail.com>
2021-11-24 14:39:27 +0200

    Solve bugs related to Slate.js breaking API change

--- src/editor/withWrapper.jsx
+++ src/editor/withWrapper.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export const withWrapper = (SlateEditor) => {
   return ({ value, ...rest }) => {
-    const ref = React.useRef();
-    if (ref.current && typeof value !== 'undefined') {
-      ref.current.children = value;
-    }
+    const ref = useRef();
+
+    useEffect(() => {
+      if (
+        ref.current // &&
+        // typeof value !== 'undefined' &&
+        // ref.current.children !== value
+      ) {
+        ref.current.children = value;
+      }
+    }, [value]);
+
     return <SlateEditor editorRef={ref} {...rest} />;
   };
 };

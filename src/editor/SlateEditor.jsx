cf831e5dd874cc6d8991ac0322b27c76cb3a843f
nileshgulia1 <nileshgulia@gmail.com>
2021-12-03 23:17:24 +0530

    rafactor onPaste,onCopy

--- src/editor/SlateEditor.jsx
+++ src/editor/SlateEditor.jsx
@@ -290,12 +290,20 @@ class SlateEditor extends Component {
                 if (handled) return;
                 onKeyDown && onKeyDown({ editor, event });
               }}
-              onPaste={(event) => {
-                if (this.props.onPaste) this.props.onPaste({ editor, event });
-              }}
-              onCopy={(event) => {
-                if (this.props.onCopy) this.props.onCopy({ editor, event });
-              }}
+              onPaste={
+                this.props.onPaste
+                  ? (event) => {
+                      this.props.onPaste({ editor, event });
+                    }
+                  : undefined
+              }
+              onCopy={
+                this.props.onCopy
+                  ? (event) => {
+                      this.props.onCopy({ editor, event });
+                    }
+                  : undefined
+              }
             />
             {selected &&
               slate.persistentHelpers.map((Helper, i) => {

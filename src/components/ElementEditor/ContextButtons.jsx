52da155e3c5d5cea555a582c46ce58f7c89686ff
Silviu Bogan <silviubogan@gmail.com>
2021-12-03 10:22:48 +0200

    Use format clear icon, make string translatable

--- src/components/ElementEditor/ContextButtons.jsx
+++ src/components/ElementEditor/ContextButtons.jsx
@@ -1,12 +1,20 @@
 import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useIntl } from 'react-intl'; // , defineMessages
+import { useIntl, defineMessages } from 'react-intl';
 
 import clearSVG from '@plone/volto/icons/delete.svg';
+import formatClearSVG from '@plone/volto/icons/format-clear.svg';
 
 import { ToolbarButton } from 'volto-slate/editor/ui';
 import { setPluginOptions } from 'volto-slate/actions';
 import { Range } from 'slate';
+
+const localMessages = defineMessages({
+  clearFormatting: {
+    id: 'Clear formatting',
+    defaultMessage: 'Clear formatting',
+  },
+});
 
 /*
  * Note: this is a weirder component, it should be called as a native function
@@ -46,12 +54,11 @@ export default (options) => (editor) => {
           );
         }}
       />
-      {Range.isExpanded(editor.selection) && (
+      {Range.isExpanded(editor.selection || editor.getSavedSelection()) && (
         <ToolbarButton
-          title="Clear formatting"
-          icon={clearSVG}
-          aria-label="Clear formatting"
-          alt="Clear formatting"
+          title={intl.formatMessage(localMessages.clearFormatting)}
+          icon={formatClearSVG}
+          aria-label={intl.formatMessage(localMessages.clearFormatting)}
           onMouseDown={doClearFormatting}
         />
       )}
